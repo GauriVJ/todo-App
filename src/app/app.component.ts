8443dc58a29f0b055a382ff3f9f6281e94c11275
GauriVJ <gvjadhav039@gmail.com>
2023-10-14 16:35:14 +0530

    sum of two numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  
+  // num1 = 0;
+  // num2 = 0;
+  // ans = 0;
+  // // today = 'mon';
+  // total(){
+  //   this.ans= this.num1 + this.num2;
+  // }((username=='admin')&&(password=='admin'))
+  username : String ='admin'
+  password : String ='admin'
+  login : boolean=false;
+   disp(){
+  
+if (this.username==='admin' && this.password==='admin') 
+{
+    this.login = true;
+} 
+else
+{
+    this.login = false; //This will be executed
+}
+   }
+  
+ 
+
+  
+ 
+
 }
