e207e1bee9f44e1bdd47aa873ae1d01f456d02f9
GauriVJ <gvjadhav039@gmail.com>
2023-10-15 15:33:03 +0530

    Made a To-Do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  err: String = '';
+  task: String = '';
+  todo: String[] = [];
+  Cindex =0;
+  isEdit = false;
+
+  add() {
+    if (this.task.trim() !== '') { // Ensure the task is not empty
+      this.todo.push(this.task);
+      this.task = ''; // Clear the input field after adding the task
+    }
+    else {
+      this.err = 'Please Enter a valid input';
+    }
+  }
+
+  remove(index: number) {
+    this.todo.splice(index, 1);
+  }
+
+  edit(index: number){
+     this.isEdit = true;
+     this.Cindex = index;
+     this.task = this.task[index];
+  }
+
+  save(){
+    this.todo[this.Cindex]=this.task;
+    this.isEdit=false;
+    this.task='';
+  }
+  cancelEdit(){
+    this.isEdit=false;
+    this.task='';
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
   // num1 = 0;
   // num2 = 0;
   // ans = 0;
@@ -14,24 +61,37 @@ export class AppComponent {
   // total(){
   //   this.ans= this.num1 + this.num2;
   // }((username=='admin')&&(password=='admin'))
-  username : String ='admin'
-  password : String ='admin'
-  login : boolean=false;
-   disp(){
-  
-if (this.username==='admin' && this.password==='admin') 
-{
-    this.login = true;
-} 
-else
-{
-    this.login = false; //This will be executed
-}
-   }
-  
- 
 
-  
- 
+
+
+
+  //   username:String | undefined
+  //   password:String | undefined
+  //   str1 =''
+  //   str2 =''
+  //   login : boolean | undefined ;
+  //    disp(){
+
+  // if (this.username =='admin' && this.password =='admin') 
+  // {
+  //     this.login = true;
+  // } 
+  // if(this.login==true){
+  //   this.str1='Login Successful'
+  // }
+  // else{
+  //   this.str2='Login Failed!'
+  // }
+
+
+
+
 
 }
+
+
+
+
+
+
+
